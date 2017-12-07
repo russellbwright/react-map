acb29aac651b610b9898af503900255e5138d0fd
Russell B W <russell.bwright@yahoo.com>
2017-12-06 23:35:16 -0600

    cleaning

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <p> This is going to be a React App with the use of ESRI's Map API </p>
-       <p> I will be using the usgs earthquake data to display recent earthquakes </p>
-        <Map />
+       
         <Side />
+        <Map />
+        
       </div>
     );
   }
